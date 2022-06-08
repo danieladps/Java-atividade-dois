4a54321278631c8608aefd23addfc440d87d79b7
Daniela Pereira <99989744+danieladps@users.noreply.github.com>
2022-06-08 19:01:35 -0300

    Update Atividade10.java

--- java_dois/Atividade10.java
+++ java_dois/Atividade10.java
@@ -1,37 +1,32 @@
 import java.util.Scanner;
-
 public class Atividade10 {
     public static void main(String[] args) {
-        //Escreva um programa que calcule a média de quatro números informados pelo usuário, mas somente se esses números forem maiores que 
-        //0 e menores que 10. No final, se a média for maior que cinco o usuário receberá uma mensagem 
-        //"Você passou no teste". Em qualquer outra situação, ele receberá uma mensagem de "tente novamente"
+        Scanner ler = new Scanner(System.in);
+        
+        int contador = 0;
+        int numeros;
+        int soma = 0;
+        
 
-        Scanner valor = new Scanner(System.in);
+        while (contador<4){
 
-        System.out.println("Digite um numero: ");
-        int n1 = valor.nextInt();
+            System.out.println("Digite um numero");
+            numeros = ler.nextInt();
+            
+            if (numeros>=0 && numeros <=10){
+            soma += numeros;
+            contador++;
 
-        System.out.println("Digite um numero: ");
-        int n2 = valor.nextInt();
-
-        System.out.println("Digite um numero: ");
-        int n3 = valor.nextInt();
-
-        System.out.println("Digite um numero: ");
-        int n4 = valor.nextInt();
-
-        int soma = n1+n2+n3+n4/4;
-
-         if(n1 > 0 && n1 <10  || n2 > 0 &&  n2 <10 || n3 > 0 && n3 <10 || n4 > 0 && n4 < 10){
-          if (soma>=5){
-                    System.out.println("Você passou no teste " + soma);
-                }else if(n1 < 0 || n2 < 0 || n3 < 0 || n4 < 0);
+            }else{
+            System.out.println("Digite um numero válido");
+            numeros = ler.nextInt();
             }
-                
-                else{
-                    System.out.println("Tente novamente " + soma);
-                }
+            
+        }
 
-     valor.close();
-      }
+            int media = soma / 4;
+
+            System.out.println("Sua média é " + media);
+            ler.close();
     }
+}
